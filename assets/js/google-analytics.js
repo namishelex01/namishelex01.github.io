fe5c0bd45ecbd34261964aa1cfcba6e3c040f569
live.namish <namishelex01@gmail.com>
2021-02-15 19:23:08 +0200

    Fixed analytics code

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GQ21V5WQKL"></script>
+<script src="https://www.googletagmanager.com/gtag/js?id=G-GQ21V5WQKL" defer></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
