6b36101f8e00dbb2c89e36322dc2dbb3bae315da
live.namish <namishelex01@gmail.com>
2021-02-15 20:04:28 +0200

    Fixed footer

--- assets/js/google-analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script src="https://www.googletagmanager.com/gtag/js?id=G-GQ21V5WQKL" defer></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-GQ21V5WQKL');
-</script>
