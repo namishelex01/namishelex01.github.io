bf0af63ced218c0c5c35ee6d379a8de20ad67041
live.namish <namishelex01@gmail.com>
2021-02-15 19:14:33 +0200

    Added analytics file

---
+++ assets/js/google-analytics.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GQ21V5WQKL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GQ21V5WQKL');
+</script>
